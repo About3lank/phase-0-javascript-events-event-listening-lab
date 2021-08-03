9c45265124ce3be7e6dbc1513f4a7ab126fde65d
Matthew Blank <matthew.3lank@gmail.com>
2021-08-03 16:09:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+
+
+    input.addEventListener('click', clickAlert);
 }
